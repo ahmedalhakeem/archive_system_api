96c05c6e22bd22d35bd144559cca005417875ffc
ahmedalhakeem <ahmkam2011@gmail.com>
2023-05-29 13:11:58 +0300

    customize db

--- archiveapp/views.py
+++ archiveapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Department
+from .serializers import DepartmentSerializer
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def departmenList(request):
+    if request.method == 'GET':
+        departments = Department.objects.all()
+        serializer = DepartmentSerializer(departments, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = DepartmentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
